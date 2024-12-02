c1b5c70808da602c6a9c2ea07cf9cf7231aac3e8
edilson591 <edilson591@gmail.com>
2024-12-02 10:15:57 -0300

    teste storage

--- src/components/ListBooks/ListBooks.tsx
+++ src/components/ListBooks/ListBooks.tsx
@@ -1,8 +1,8 @@
+import { useEffect } from "react";
 import { useGetBooksQuery } from "../../services/booksApi";
 import { Books } from "../../services/interfaceBooks";
 import { ComponentBook } from "../Book/Book";
 import BookFiltered from "../BookFiltered/BookFiltered";
-
 
 export interface ListBooksProps {
   filters: { search: string; category: string };
@@ -12,15 +12,17 @@ export interface ListBooksProps {
 
 export function ListBooks({ filters, favoritos, isFavorites }: ListBooksProps) {
   const { data, isLoading, isError } = useGetBooksQuery();
+
+  useEffect(() => {
+    localStorage.setItem("favorites", JSON.stringify(favoritos));
+  }, [favoritos]);
+
   if (isLoading) return <h2>...Carregando</h2>;
   if (isError) return <h2>Ocorreu um erro ao carregar os livros</h2>;
 
   const booksToFilter = isFavorites ? favoritos : data;
 
   const filteredBook = BookFiltered(booksToFilter, filters);
-
-  
-  
 
   return (
     <section className="mb-8">
