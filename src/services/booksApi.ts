66597118a6ee8c5d61af1dbab9880d6ce29a1472
edilson591 <edilson591@gmail.com>
2024-12-02 15:34:17 -0300

    add information

--- src/services/booksApi.ts
+++ src/services/booksApi.ts
@@ -4,7 +4,7 @@ import { Books } from "./interfaceBooks";
 const booksApi = createApi({
   reducerPath: "booksApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:1000/",
+    baseUrl: "https://crud-book-ng42.onrender.com",
   }),
   endpoints: (builder) => ({
     getBooks: builder.query<Books[], void>({
